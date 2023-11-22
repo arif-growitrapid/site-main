f228e69f4c0d64924c8d20b994efa65bb4791607
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-11-22 22:43:08 +0530

    modified styles and fixed responsiveness bugs

--- src/app/(public)/blogs/components/BlogCard.tsx
+++ src/app/(public)/blogs/components/BlogCard.tsx
@@ -39,62 +39,82 @@ const BlogCard: React.FC<BlogCardProps> = ({ blogs, type }) => {
 
   if (type === 'cards') {
     return (
-      <div className={style.popularBlogs}>
-        <SkeletonTheme baseColor='var(--bg-color)' highlightColor='var(--tertiary-color)'>
-          {blogs?.map((element, index) => (
-            <div key={index} className={`${style.horizontalCards}`}>
-              {element ? (
-                <Image width={1080} height={720} src={element.thumbnail} className={style.thumbnail} alt={element.title} />
-              ) : (
-                <Skeleton height={'100%'} containerClassName={style.thumbnail} />
-              )}
+      <>
+        <div className={style.title}>
+          <div>
+            <svg className={style.trending} width="50" viewBox="0 0 90 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M82.497 2.50001C83.1716 2.49863 83.8393 2.63515 84.4592 2.90116C85.6673 3.41514 86.6249 4.38376 87.125 5.59795L87.1361 5.62514L87.1467 5.65257L87.18 5.73924L87.1856 5.75378L87.191 5.76839C87.3952 6.31959 87.5 6.9105 87.5 7.50001V30C87.5 31.3261 86.9732 32.5978 86.0355 33.5355C85.0979 34.4732 83.8261 35 82.5 35C81.1739 35 79.9021 34.4732 78.9645 33.5355C78.0268 32.5978 77.5 31.3261 77.5 30V19.5722L52.506 40.4266C52.5042 40.4267 52.5025 40.4267 52.5007 40.4267C51.1867 40.4309 49.8848 40.1743 48.6706 39.6718C47.4559 39.1691 46.3531 38.4303 45.4261 37.4983C45.4252 37.4974 45.4243 37.4965 45.4235 37.4956L35.0011 27.0733L35 27.0734L34.9988 27.0734L11.0424 51.0264C11.0408 51.028 11.0392 51.0297 11.0375 51.0313C10.574 51.4977 10.0227 51.8677 9.41543 52.1197C8.80832 52.3718 8.15731 52.501 7.5 52.5C6.84269 52.501 6.19168 52.3718 5.58457 52.1197C4.97843 51.8681 4.42805 51.4991 3.96511 51.034C3.0194 50.0914 2.5 48.824 2.5 47.5C2.5 46.1744 3.02063 44.9056 3.96849 43.9626C3.9696 43.9615 3.97071 43.9604 3.97182 43.9593L27.9289 20.0022C29.8169 18.1142 32.3371 17.0767 35.0033 17.0767C37.6715 17.0767 40.1912 18.119 42.0744 20.0022L52.5011 30.4289L52.5033 30.4311L52.5055 30.4289L52.5056 30.4289L63.6039 19.3306L70.4345 12.5M82.497 2.50001H82.5V5.00001L82.4927 2.50002C82.4942 2.50002 82.4956 2.50001 82.497 2.50001ZM82.497 2.50001H60C58.6739 2.50001 57.4021 3.02679 56.4645 3.96448C55.5268 4.90215 55 6.17392 55 7.50001C55 8.8261 55.5268 10.0979 56.4645 11.0355C57.4021 11.9732 58.6739 12.5 60 12.5H70.4345M70.4345 12.5H73.97L72.9345 10L70.4345 12.5ZM34.9952 27.0674L34.9953 27.0675C34.9953 27.0675 34.9952 27.0674 34.9952 27.0674Z" fill="white" stroke="black" stroke-width="5" />
+            </svg>
+            <h1 className='flex items-end text-3xl text-center md:text-left leading-tight font-black text-[var(--dark-text-color)] md:text-current'>
+              Popular Blogs:-
+            </h1>
+          </div>
 
-              <div className={style.content}>
-                <div className={style.profileContainer}>
+          <div className={style.navigation}>
+            <div ref={navigationPrevRef}>
+              <AiFillCaretLeft className={style.prevBtn} />
+            </div>
+            <div ref={navigationNextRef}>
+              <AiFillCaretRight className={style.nextBtn} />
+            </div>
+          </div>
+        </div>
+        <div className={style.popularBlogs}>
+          <SkeletonTheme baseColor='var(--bg-color)' highlightColor='var(--tertiary-color)'>
+            {blogs?.map((element, index) => (
+              <div key={index} className={`${style.horizontalCards}`}>
+                {element ? (
+                  <Image width={1080} height={720} src={element.thumbnail} className={style.thumbnail} alt={element.title} />
+                ) : (
+                  <Skeleton height={'100%'} containerClassName={style.thumbnail} />
+                )}
+
+                <div className={style.content}>
+                  <div className={style.profileContainer}>
+                    {element ? (
+                      <Image className={style.profilePic} width={50} height={50} src={element.author.image} alt='profile pic' />
+                    ) : (
+                      <Skeleton width={50} height={50} circle={true} style={{ marginRight: '1em' }} containerClassName={style.profilePic} />
+                    )}
+
+                    <div className={style.profileInfo}>
+                      <div className={style.authorName}>{element?.author.name || <Skeleton width={`${Math.floor(Math.random() * (150 - 50 + 1)) + 50}px`} />}</div>
+                      <div className={style.subtext}>{element?.author.email || <Skeleton width={`${Math.floor(Math.random() * (150 - 50 + 1)) + 50}px`} />}</div>
+                    </div>
+                  </div>
+
                   {element ? (
-                    <Image className={style.profilePic} width={50} height={50} src={element.author.image} alt='profile pic' />
+                    <>
+                      <h2>{element?.title}</h2>
+                      <p className={style.desc}>{element?.excerpt}</p>
+                    </>
                   ) : (
-                    <Skeleton width={50} height={50} circle={true} style={{ marginRight: '1em' }} containerClassName={style.profilePic} />
+                    <>
+                      <Skeleton width={'100%'} height={'2em'} containerClassName={style.text} />
+                      <Skeleton width={`${Math.floor(Math.random() * (100 - 20 + 1)) + 20}%`} height={'2em'} containerClassName={style.text} />
+                      <Skeleton width={'100%'} height={'1em'} containerClassName={style.text} />
+                      <Skeleton width={`${Math.floor(Math.random() * (100 - 20 + 1)) + 20}%`} height={'1em'} containerClassName={style.text} />
+                    </>
                   )}
 
-                  <div className={style.profileInfo}>
-                    <div className={style.authorName}>{element?.author.name || <Skeleton width={`${Math.floor(Math.random() * (150 - 50 + 1)) + 50}px`} />}</div>
-                    <div className={style.subtext}>{element?.author.email || <Skeleton width={`${Math.floor(Math.random() * (150 - 50 + 1)) + 50}px`} />}</div>
-                  </div>
+                  {element ? (
+                    <Link href={`/blogs/${element?.slug}`} className={style['learn_more_btn']}>
+                      <div>
+                        <span>Read Now</span>
+                      </div>
+                      <div>
+                        <FaArrowRight className={`inline-block ml-2`} />
+                      </div>
+                    </Link>
+                  ) : (
+                    <Skeleton containerClassName={style['learn_more_btn']} />
+                  )}
                 </div>
-
-                {element ? (
-                  <>
-                    <h2>{element?.title}</h2>
-                    <p className={style.desc}>{element?.excerpt}</p>
-                  </>
-                ) : (
-                  <>
-                    <Skeleton width={'100%'} height={'2em'} containerClassName={style.text} />
-                    <Skeleton width={`${Math.floor(Math.random() * (100 - 20 + 1)) + 20}%`} height={'2em'} containerClassName={style.text} />
-                    <Skeleton width={'100%'} height={'1em'} containerClassName={style.text} />
-                    <Skeleton width={`${Math.floor(Math.random() * (100 - 20 + 1)) + 20}%`} height={'1em'} containerClassName={style.text} />
-                  </>
-                )}
-
-                {element ? (
-                  <Link href={`/blogs/${element?.slug}`} className={style['learn_more_btn']}>
-                    <div>
-                      <span>Read Now</span>
-                    </div>
-                    <div>
-                      <FaArrowRight className={`inline-block ml-2`} />
-                    </div>
-                  </Link>
-                ) : (
-                  <Skeleton containerClassName={style['learn_more_btn']} />
-                )}
               </div>
-            </div>
-          ))}
-        </SkeletonTheme>
-      </div>
-
+            ))}
+          </SkeletonTheme>
+        </div>
+      </>
     );
   } else if (type === "carousel") {
     return (
