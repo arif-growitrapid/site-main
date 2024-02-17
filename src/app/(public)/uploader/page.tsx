d397d56b09b78dab61c9b8c1fba11a6dd3702680
Srinivasan-Akash <prashanthiakash@gmail.com>
2024-02-17 20:40:01 +0530

    Reading excel file & showing data

--- src/app/(public)/uploader/page.tsx
+++ src/app/(public)/uploader/page.tsx
@@ -1,34 +1,36 @@
 'use client'
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import * as XLSX from 'xlsx';
 
-export default function Page() {
-    const onDrop = useCallback((acceptedFiles) => {
+const Page: React.FC = () => {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
         const file = acceptedFiles[0];
         if (file) {
             parseLinks(file);
         }
     }, []);
 
-    const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: '.csv' });
+    const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: { "application/vnd.ms-excel": [".csv", ".xlsx", ".xls"] } });
+    const [fileUploaded, setFileUploaded] = useState<File[]>();
 
-    const parseLinks = (file) => {
+    const parseLinks = (file: File) => {
         const reader = new FileReader();
         reader.onload = (event) => {
-          const arrayBuffer = event.target.result;
-          const workbook = XLSX.read(arrayBuffer, { type: 'array' });
-          const sheetName = workbook.SheetNames[0];
-          const worksheet = workbook.Sheets[sheetName];
-          const data = XLSX.utils.sheet_to_json(worksheet);
-          console.log(data);
+            const arrayBuffer = event.target?.result as ArrayBuffer;
+            const workbook = XLSX.read(arrayBuffer, { type: 'array' });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const data = XLSX.utils.sheet_to_json(worksheet);
+            setFileUploaded(data as File[])
+            console.log(data as File[])
         };
         reader.readAsArrayBuffer(file);
     };
 
     return (
-        <section className="flex items-center justify-center h-full w-full">
-            <div {...getRootProps()} className="shadow-lg flex items-center justify-center w-[80%] aspect-video">
+        <section className="flex items-center justify-center h-full w-full gap-1">
+            <div {...getRootProps()} className="shadow-lg flex items-center justify-center w-[40%] h-[60%]">
                 <label
                     htmlFor="dropzone-file"
                     className="flex flex-col items-center justify-center w-full h-full border-2 border-[#20344D] border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
@@ -43,9 +45,9 @@ export default function Page() {
                         >
                             <path
                                 stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
                                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                             />
                         </svg>
@@ -57,6 +59,30 @@ export default function Page() {
                     <input {...getInputProps()} />
                 </label>
             </div>
+            <div className='h-[60%] w-[40%] bg-black shadow-lg rounded-lg'>
+                {fileUploaded ? (
+                    <table className=" text-white">
+                        <thead>
+                            <tr>
+                                {Object.keys(fileUploaded[0]).map((header, index) => (
+                                    <th key={index}>{header}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {fileUploaded.map((row, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    {Object.values(row).map((value, colIndex) => (
+                                        <td key={colIndex}>{value}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : "NOPE"}
+            </div>
         </section>
     );
-}
+};
+
+export default Page;
