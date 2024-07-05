db3a157dc99b8d07d4289030e6e566c9cd6e98c4
Arif Sardar <as2048282@gmail.com>
2024-07-05 15:28:25 +0530

    updated shown courses

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -44,12 +44,12 @@ export default function Page({}: {}) {
         "Dive into the dynamic world of web development, a crucial skill sought after in today's tech-driven landscape. From crafting responsive websites to mastering the latest frameworks, our web development courses empower you to thrive in the digital realm. ",
       courses: [
         {
-          link: "",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194165447889797191/Frame_2.png?ex=65af5c3d&is=659ce73d&hm=1f129c23fa02436f6e57d24bb26f55920aee89508e7e309a461d8a67bd34afa9&=&format=webp&quality=lossless&width=967&height=544",
+          link: "https://imp.i384100.net/eKJkVZ",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720172700/uploaded/Frame_4_qu5i8l.webp",
         },
         {
-          link: "",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194165447889797191/Frame_2.png?ex=65af5c3d&is=659ce73d&hm=1f129c23fa02436f6e57d24bb26f55920aee89508e7e309a461d8a67bd34afa9&=&format=webp&quality=lossless&width=967&height=544",
+          link: "https://imp.i384100.net/6e9YQQ",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720172870/uploaded/frontend_developer_uvi7n3.webp",
         },
       ],
     },
@@ -60,24 +60,24 @@ export default function Page({}: {}) {
         "Dive into the dynamic world of data analysis, a crucial skill sought after across diverse industries such as finance, transportation, education, manufacturing, and banking. Our courses cover essential aspects of data science, incorporating Python, statistics, and machine learning to enhance your expertise.",
       courses: [
         {
+          link: "https://imp.i384100.net/WqaMyG",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720173086/uploaded/maths_for_data_science_wiibbq.webp",
+        },
+        {
           link: "https://imp.i384100.net/jr549M",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194158971217514516/Frame_1.png?ex=65af5635&is=659ce135&hm=f2d83b32e7cb0f6ec29a4dc81afd034a845a917d308d09d957ad63e831fe3481&=&format=webp&quality=lossless&width=967&height=544",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720172785/uploaded/Frame_1_1_ud9kll.webp",
         },
         {
-          link: "https://imp.i384100.net/5g07zj",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194165447889797191/Frame_2.png?ex=65af5c3d&is=659ce73d&hm=1f129c23fa02436f6e57d24bb26f55920aee89508e7e309a461d8a67bd34afa9&=&format=webp&quality=lossless&width=967&height=544",
+          link: "https://imp.i384100.net/5gQvML",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720173304/uploaded/HUMAN_RESOURCE_wvlxkg.webp",
         },
         {
-          link: "",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194165447889797191/Frame_2.png?ex=65af5c3d&is=659ce73d&hm=1f129c23fa02436f6e57d24bb26f55920aee89508e7e309a461d8a67bd34afa9&=&format=webp&quality=lossless&width=967&height=544",
+          link: "https://imp.i384100.net/xkogKd",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720173405/uploaded/digital_product_management_q1cvtz.webp",
         },
         {
-          link: "",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194165447889797191/Frame_2.png?ex=65af5c3d&is=659ce73d&hm=1f129c23fa02436f6e57d24bb26f55920aee89508e7e309a461d8a67bd34afa9&=&format=webp&quality=lossless&width=967&height=544",
-        },
-        {
-          link: "",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194165447889797191/Frame_2.png?ex=65af5c3d&is=659ce73d&hm=1f129c23fa02436f6e57d24bb26f55920aee89508e7e309a461d8a67bd34afa9&=&format=webp&quality=lossless&width=967&height=544",
+          link: "https://imp.i384100.net/0Z7vMO",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720173436/uploaded/DATA_BASE_ENGINEERING_g5ksvw.webp",
         },
       ],
     },
@@ -88,8 +88,8 @@ export default function Page({}: {}) {
         "Leadership is a universally sought-after skill that transcends industries. From finance to education, manufacturing to banking, strong leadership is pivotal for success. Our comprehensive leadership courses empower you with essential skills, equipping you for impactful decision-making and effective team management.",
       courses: [
         {
-          link: "https://imp.i384100.net/daJ7PK",
-          img: "https://media.discordapp.net/attachments/1144663357845147790/1194169277608120431/Frame_4.png?ex=65af5fce&is=659ceace&hm=bc4ea1a2a90641aee10843dddf79b49e9a5a5c754527f24fceb6de284e525c15&=&format=webp&quality=lossless&width=967&height=544",
+          link: "https://imp.i384100.net/oq3kX9",
+          img: "https://res.cloudinary.com/djoo8ogmp/image/upload/v1720173346/uploaded/english_jd2vkf.webp",
         },
       ],
     },
@@ -487,7 +487,7 @@ export default function Page({}: {}) {
               </button>
             </div>
           </div>
-      
+
           <div className={style.timeline}>
             <VerticalTimeline>
               {workflow.map((experience, index) => (
